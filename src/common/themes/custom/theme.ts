a640ce9e8983e44f24c94664775a7df6b6e9659d
Phạm Tuấn Sơn <phamtuanson2000@gmail.com>
2022-03-15 01:26:46 +0700

    fix mobile layout

--- src/common/themes/custom/theme.ts
+++ src/common/themes/custom/theme.ts
@@ -2,65 +2,68 @@ import {
   darkBackground,
   grey,
   lightBackground,
-  primary
+  primary,
 } from "#/styles/colors";
 import { createTheme, ThemeOptions } from "@mui/material/styles";
 import _ from "lodash";
 import {
   CommonMuiAppBar,
   DarkModeMuiAppBar,
-  LightModeMuiAppBar
+  LightModeMuiAppBar,
 } from "./appBar";
 import { breakpoints } from "./breakpoints";
+
+// Add customization import (do not delete this comment)
+import { CommonMuiCard, DarkModeMuiCard, LightModeMuiCard } from "./card";
 import {
   CommonMuiButton,
   DarkModeMuiButton,
-  LightModeMuiButton
+  LightModeMuiButton,
 } from "./button";
-import { CommonMuiCard, LightModeMuiCard } from "./card";
 import {
-  CommonMuiCardContent, LightModeMuiCardContent
+  CommonMuiPagination,
+  DarkModeMuiPagination,
+  LightModeMuiPagination,
+} from "./pagination";
+import {
+  CommonMuiCardContent,
+  DarkModeMuiCardContent,
+  LightModeMuiCardContent,
 } from "./cardContent";
 import {
   CommonMuiCardMedia,
   DarkModeMuiCardMedia,
-  LightModeMuiCardMedia
+  LightModeMuiCardMedia,
 } from "./cardMedia";
-// Add customization import (do not delete this comment)
 import {
-  CommonMuiContainer, LightModeMuiContainer
+  CommonMuiContainer,
+  DarkModeMuiContainer,
+  LightModeMuiContainer,
 } from "./container";
 import {
   CommonMuiIconButton,
   DarkModeMuiIconButton,
-  LightModeMuiIconButton
+  LightModeMuiIconButton,
 } from "./iconButton";
 import {
   CommonMuiInputBase,
   DarkModeMuiInputBase,
-  LightModeMuiInputBase
+  LightModeMuiInputBase,
 } from "./inputBase";
 import { CommonMuiLink, DarkModeMuiLink, LightModeMuiLink } from "./link";
 import {
   CommonMuiMenuItem,
   DarkModeMuiMenuItem,
-  LightModeMuiMenuItem
+  LightModeMuiMenuItem,
 } from "./menuItem";
-import {
-  CommonMuiPagination,
-  DarkModeMuiPagination,
-  LightModeMuiPagination
-} from "./pagination";
 import { CommonMuiPaper, DarkModeMuiPaper, LightModeMuiPaper } from "./paper";
 import {
   CommonMuiToggleButton,
   DarkModeMuiToggleButton,
-  LightModeMuiToggleButton
+  LightModeMuiToggleButton,
 } from "./toggleButton";
 import { CommonMuiToggleButtonGroup } from "./toggleButtonGroup";
 import { CommonMuiToolbar } from "./toolbar";
-
-
 
 const commonThemeOptions: ThemeOptions = {
   palette: {
@@ -75,8 +78,8 @@ const commonThemeOptions: ThemeOptions = {
   breakpoints,
   components: {
     // Add common component customization (do not delete this comment)
-    MuiPagination: CommonMuiPagination,
     MuiContainer: CommonMuiContainer,
+    MuiPagination: CommonMuiPagination,
     MuiCardContent: CommonMuiCardContent,
     MuiCardMedia: CommonMuiCardMedia,
     MuiToolbar: CommonMuiToolbar,
@@ -118,7 +121,10 @@ const darkModeOptions: ThemeOptions = {
   components: {
     // Add dark mode component customization (do not delete this comment)
     MuiPagination: DarkModeMuiPagination,
+    MuiContainer: DarkModeMuiContainer,
+    MuiCardContent: DarkModeMuiCardContent,
     MuiCardMedia: DarkModeMuiCardMedia,
+    MuiCard: DarkModeMuiCard,
     MuiLink: DarkModeMuiLink,
     MuiInputBase: DarkModeMuiInputBase,
     MuiPaper: DarkModeMuiPaper,
--- src/containers/PersonalReview/PersonalReview.tsx
+++ src/containers/PersonalReview/PersonalReview.tsx
@@ -2,21 +2,14 @@ import { useIsMobile } from "@/common/styles/responsive";
 import React from "react";
 import ComputerPersonalReview from "./ComputerPersonalReview";
 import MobilePersonalReview from "./MobilePersonalReview";
-interface PersonalReviewProps { }
+interface PersonalReviewProps {}
 
-const PersonalReview: React.FC<PersonalReviewProps> = ({ }) => {
-    const isMobile = useIsMobile();
+const PersonalReview: React.FC<PersonalReviewProps> = ({}) => {
+  const isMobile = useIsMobile();
 
-    return (
-        <>
-            {
-                isMobile ?
-                    <MobilePersonalReview />
-                    :
-                    <ComputerPersonalReview />
-            }
-        </>
-    );
+  return (
+    <>{isMobile ? <MobilePersonalReview /> : <ComputerPersonalReview />}</>
+  );
 };
 
 export default PersonalReview;
